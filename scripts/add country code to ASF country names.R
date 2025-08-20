3eade817539515833e431415892f2f80f5c5984b
dalon-white <dalon.white@usda.gov>
2025-08-20 11:55:22 -0400

    Updated wads volume reference table, organized files, and started putting aqim data layers together

--- scripts/add country code to ASF country names.R
+++
@@ -1,9 +0,0 @@
-#### if needed to add 3 digit country codes to a list of ASF affected countries, run this #####
-filename = "ASF affected countries 2009 to 2020 McKee et al 2023.csv"
-
-setwd(here::here('input','data','asf affected countries by year'))
-df <- read.csv(filename)
-
-df$Country_name = countrycode::countrycode(df$Country, destination = "country.name", origin = "iso3c")
-
-write.csv(df, filename)
